ba79f2db7a161f54bfb0b58ba5d0d25f644eb550
Shubham Pandey <shubhpan1996@gmail.com>
2025-12-30 23:46:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'filmyfool-v14'; // Increment this every time you push a change
+const CACHE_NAME = 'filmyfool-v15'; // Increment this every time you push a change
 const ASSETS = [
   './',
   './index.html',
